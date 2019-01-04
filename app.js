94f20a5bc824c766c52e31c8fbb08b3ffa3472c3
David Ezihe <ue18aac@herts.ac.uk>
2019-01-04 20:49:02 +0000

    Bootstrap installed using bower front-end package manager.

--- app.js
+++ app.js
@@ -28,9 +28,12 @@ app.set('view engine', 'pug');
 
 //Body parser middleware
 //parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Home Route
 app.get('/', function(req, res){
