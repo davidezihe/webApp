722d2502fd0cafe250896dd1c31d51901a867f8f
David Ezihe <ue18aac@herts.ac.uk>
2019-01-04 21:48:37 +0000

    Edit button created and routed. "edit_article.pug" view created.

--- app.js
+++ app.js
@@ -82,6 +82,35 @@ app.post('/articles/add', function(req, res){
   });
 });
 
+//Load Edit Form
+app.get('/article/edit/:id', function(req, res){
+  Article.findById(req.params.id, function(err, article){
+    res.render('edit_article', {
+      title:'Edit Article',
+      article:article
+    });
+  });
+});
+
+//Update Submit POST Route
+app.post('/articles/edit/:id', function(req, res){
+  let article = {};
+  article.title = req.body.title;
+  article.author = req.body.author;
+  article.body = req.body.body;
+
+  let query = {_id:req.params.id}
+
+  Article.update(query, article, function(err){
+    if(err){
+      console.log(err);
+      return;
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
 //Start Server
 app.listen(3000, function(){
   console.log('Server started on port 3000.');
